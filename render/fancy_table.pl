26b5b10f4d77cd160fbe8a520a075e52015d4c84
Jozef Geurts <jpt.geurts@gmail.com>
2020-02-24 21:39:12 +0100

    wip

--- render/fancy_table.pl
+++ render/fancy_table.pl
@@ -15,75 +15,171 @@
 :- debug(fancy_table,"Fancy table renderer loaded",[]).
 
 :- register_renderer(fancy_table, "Fancy options for rendering tables").
+:- use_module(swish(lib/storage)).
+
+
+% :- use_gitty_file(test_fancy_term). % this comes from SWISH
 
 
 /** <module> SWISH fancy table renderer
-Includes more fancy options for rendering tables of dicts than the standard table renderer
+Includes fancy options for rendering tables using the standard table renderer
 
 Options include:
+	- multuple key sorting
+	- special formatting for specific values
+	- download as xls 
 
-sorting, download csv
-*/
-
-/*
-table_spec([
-       		_{tag:capital, keys:[name, country],sort_keys:[country], order:ascending}
-           ]),
-     export(csv),
-     count(true)
 */
 
 
-%%	term_rendering(+Dicts:list, +Vars:list, +Options:list)//
+%%	term_rendering(+Rows:list, +Vars:list, +Options:list)//
 %
-%	Renders  Dicts as a fancy_table.
-
-term_rendering([], _Vars, _Options) -->
-	!,
-	html(div([ style('display:inline-block'),
-		   'data-render'('Fancy Table')
-		 ],
-		 [  
-		 	\fancy_table_footer([])
-		 ])).
-
-term_rendering(Dicts, Vars, Options) -->
+%	Renders Rows as a fancy_table.
+% 	Supported formats
+%   Rows = [[2,2,1],[1,2,3]]. 
+%   Rows = [[population(1),name("aaa"),country("fr")]] 
+%   Rows = [[population-1,name - "aaa",country - "fr"]]
+%   Rows = [[population=1,name="aaa",country="fr"]]
+%   Rows = [a(2,2,1),a(1,2,3)]
+%   Rows = [captial{populaton:1,name:"aa",country:"fr"}]
+%   NRows = [capital(population-1,name-"aaa",country-"fr"),]
+term_rendering(Term, _Vars, Options) -->
 	{   
-		% debug(fancy_table, "Dicts: ~w, Vars:~w, Options:~w",[Dicts,Vars,Options]),
-		is_list_of_dicts(Dicts,Tag,DictKeys),
-		debug(fancy_table, "Tag: ~w, AllKeys:~w, Options:~w",[Tag, DictKeys,Options]),
-
-		% select the renderer table_spec to use		
-		select(table_spec(Specs),Options, NOptions),
-		member(Spec,Specs),
-		Spec =.. [Tag,SortKeys],
-		findall(Key,(member(TmpKey,SortKeys),(TmpKey =.. [Key,_] -> true ; Key = TmpKey)),Keys),
-		subset(Keys,DictKeys),!,
-		
-		sort_dicts(Dicts,SortKeys,SortedDicts),
-		dicts_to_rows(SortedDicts,Keys,Rows)
+		debug(fancy_table, "try fancy_table with header ~p",[Term]),
+		normalise_rows(Term,Rows),
+		debug(fancy_table, "normalised ~w",[Rows]),
+		match_header(Options,Rows,Header),!,
+		debug(fancy_table, "use fancy table - header:~w, options:~w",[Header,Options]),
+		forall(member(fancy_file(GittyFile),Options),use_gitty_file(GittyFile)),
+		Header =.. [_|SortKeys],
+		sort_rows(Rows,SortKeys,SortedRows),
+		debug(fancy_table,"sorted rows:~p",[SortedRows]),
+		qualify_rows(SortedRows,QRows)
 	},
-	fancy_table_rendering(Tag,Keys,Rows,Vars,NOptions).
+	fancy_table_rendering(Header,QRows,Options).
 
-term_rendering(Dicts, Vars, Options) -->
-	{
-		is_list_of_dicts(Dicts,Tag,Keys),
-		dicts_to_rows(Dicts,Keys,Rows)
+term_rendering(Term, _Vars, Options) -->
+	{   
+		debug(fancy_table, "try fancy_table without header",[]),
+		normalise_rows(Term,Rows),
+		qualify_rows(Rows,QRows),
+		debug(fancy_table,"~p, ~w ",[Rows,QRows])
 	},
-	fancy_table_rendering(Tag,Keys,Rows,Vars,Options).
+	fancy_table_rendering(QRows,Options).
 
-fancy_table_rendering(Tag,Keys,Rows,Vars,Options) -->
+qualify_rows([],[]).
+qualify_rows([Row|Rows],[QRow|QRows]) :-
+	Row =.. [Tag|Pairs],
+	debug(fancy_table,"oairs:~w",[Pairs]),
+	findall(QValue,(member(K-V,Pairs),QValue =.. [K,V]),QValues),
+	QRow =..[Tag|QValues],
+	qualify_rows(Rows,QRows).
+
+normalise_rows([],[]).
+normalise_rows([Row|Rows],[NRow|NRows]) :-
+	normalise_row(Row,NRow),
+	normalise_rows(Rows,NRows).
+
+normalise_row(Dict,NRow) :-
+	is_dict(Dict),!,
+	dict_pairs(Dict,Tag,Pairs),
+	(var(Tag) -> NTag = row ; NTag = Tag), 
+	NRow =.. [NTag|Pairs].
+
+normalise_row(Row,NRow) :-
+	qpairs(Row,NRow).
+
+qpairs(Row,NRow) :-
+	\+ is_list(Row),
+	compound(Row),!,
+	Row =.. [T|Pairs],
+	pairs(Pairs,NPairs),
+	NRow =.. [T|NPairs].
+
+qpairs(Pairs,row(NPairs)) :-
+	is_list(Pairs),
+	pairs(Pairs,NPairs). 
+
+pairs([],[]).
+pairs([Pair|Pairs],[NPair|NPairs]) :-
+	pair(Pair,NPair),
+	pairs(Pairs,NPairs).
+
+pair(K-V,K-V) :- !. 
+pair(K=V,K-V) :- !. 
+pair(K:V,K-V) :- !. 
+pair(Pair,K-V) :-
+	compound(Pair),
+	Pair =.. [K,V],!.
+pair(V,value-V). 
+
+%! match_header(+Options,+Rows,-Header) is nondet
+match_header(Options,Rows,Header) :-
+	common_keys(Rows,CommonKeys),
+	member(header(Header),Options),
+	header_keys(Header,Tag,HeaderKeys),
+	subset(HeaderKeys,CommonKeys),
+	forall(member(Row,Rows), Row =.. [Tag|_]).
+
+common_keys([],[]).
+common_keys([Row],Keys) :-
+	Row =.. [_|Pairs],
+	pairs_keys(Pairs,Keys).
+common_keys([Row|Rows],CommonKeys) :-
+	Row =.. [_|Pairs],
+	pairs_keys(Pairs,Keys),
+	common_keys(Rows,CKeys),
+	intersection(Keys,CKeys,CommonKeys).
+
+header_keys(Header,Tag,HeaderKeys) :-
+	Header =.. [Tag|SortKeys],
+	findall(HKey,(member(SortKey,SortKeys),SortKey =.. [HKey,_]),HeaderKeys).
+
+
+% sort_rows([row(2,1,3),row(1,1,1),row(1,2,2)],[a(@=<),b(@>=),c(_)],Sorted).
+sort_rows(Rows,SortKeys,Sorted) :-
+	sort_rows(Rows,0,SortKeys,Sorted).
+
+sort_rows(Rows,N,SortKeys,Rows) :- length(SortKeys,N),!.
+sort_rows(Rows,I,SortKeys,Sorted) :-
+	nth1(I,SortKeys,SortKey),
+	SortKey =.. [_Key,Order],
+	nonvar(Order),!,
+	J is I + 1,
+	sort_rows(Rows,J,SortKeys,TmpSorted),
+	sort(I,Order,TmpSorted,Sorted).
+
+sort_rows(Rows,I,SortKeys,Sorted) :-
+	J is I + 1,
+	sort_rows(Rows,J,SortKeys,Sorted).	
+
+
+fancy_table_rendering(Header,Rows,Options) -->
 	{
-		debug(fancy_table, "Tag: ~w, Keys:~w, Rows:~w",[Tag, Keys, Rows]),
-		Header =.. [Tag|Keys]
+		header_keys(Header,Tag,Keys),
+		NHeader =.. [Tag|Keys]
+		% debug(fancy_table, "Tag: ~w, Keys:~w, Rows:~w, Options:~w, Header:~w",[Tag, Keys, Rows, Options,Header])
 	},
 	html(div([class(['export-dom']), style('display:inline-block'),
-		   'data-render'('Fancy Table')
+		   'data-render'('Fancy Table with Header')
 		 ],
 		 [  
-		 	\table_term_rendering(Rows,Vars,[keys(Keys),header(Header)|Options]),
+		 	\table_term_rendering(Rows,_,[header(NHeader)|Options]),
 		 	\fancy_table_footer(Rows)		 	
 		 ])).
+
+fancy_table_rendering(Rows,Options) -->
+	{
+		% debug(fancy_table, "Tag: ~w, Keys:~w, Rows:~w, Options:~w, Header:~w",[Tag, Keys, Rows, Options,Header])
+	},
+	html(div([class(['export-dom']), style('display:inline-block'),
+		   'data-render'('Fancy Table without Header')
+		 ],
+		 [  
+		 	\table_term_rendering(Rows,_,Options),
+		 	\fancy_table_footer(Rows)		 	
+		 ])).
+
 
 fancy_table_footer([]) --> html(p("0 records")).
 fancy_table_footer(Rows) -->
@@ -93,7 +189,7 @@ fancy_table_footer(Rows) -->
 		to_csv(Rows,CSV,[]),
 		Rows = [R|_], R =.. Tag, 
 		format(string(DataURI),"data:~w,~w",["text/csv",CSV]),
-		format(string(FName),"~w.csv",Tag)
+		format(string(FName),"~w.csv",[Tag])
 
 	},
 	html([p([FooterMesg," ",a([href(DataURI),download(FName)],"(export)")])]).
@@ -121,99 +217,62 @@ atomize_row(Row,ARow) :-
 %%	fancy_term(Term, +Options)
 %
 %	Specific renders for values in the table
-% 	Term is of the form key(value) (eg 'wiki_id("Projet H2020 341"))'') 
 %	
 %	supported;
 % 	we renders dates as eg "16 Aug 2018"
 %	wiki_id include a clcikable link 
+%  	human friendly format for dates
 
-% human readible format for dates
-fancy_term(Term, Options) -->
+:- multifile swish_render_fancy_table:fancy_term/5.
+
+user_or_default_fancy_term(ColName,Cell,Options) -->  {debug(fancy_table,"col:~w, cell:~w, ops: ~w",[ColName,Cell,Options])},fancy_term(ColName,Cell,Options).
+user_or_default_fancy_term(ColName,Cell,Options) --> def_fancy_term(ColName,Cell,Options).
+
+def_fancy_term(_,Date, Options) -->
 	{   
-		%debug(fancy_table, "fancy_term: Term: ~w",[Term]),
-		Term =.. [_,Date],
 		Date = date(_,_,_),!,
 		format_time(string(DateStr),"%d %b %Y",Date)
 	},
 	html(td(\term(DateStr, Options))).
 
-% include a link for wiki_id
-fancy_term(wiki_id(WikiId), _Options) -->
-	{   
-		format(atom(URL),"https://wiki.inria.fr/dpe/~w",[WikiId])
-	},
-	html(td(a(href(URL),WikiId))).
-
-% include a link for uri
-fancy_term(uri(URI), _Options) -->
-	{   
-	},
-	html(td(a(href(URI),URI))).
-
 % use label for IRIs if defined
-fancy_term(property(IRI), Options) -->
+def_fancy_term(_,IRI, Options) -->
 	{ 
-		rdf_global_id(IRI,URI),
-		debug(fancy_table,"iri: ~w, uri:~w", [IRI,URI]),
-		rdfs_label(URI,Label)  
+		atom(IRI),
+		rdf_iri(IRI),
+		rdfs_label(IRI,Label), 
+		debug(fancy_table,"iri: ~w, label:~w", [IRI,Label])
 	},
 	html(td(\term(Label, Options))).
 
-%%	sort_dicts(+In:list, +Keys:list, +Sorted:list)//
-%
-%	Sorts a list of dicts on multiple keys
-%   keys are of the format:  key(=<), or key
-%
-%   eg sort_dicts([_{a:2,b:1,c:3},_{a:1,b:1,c:1},_{a:1,b:2,c:2}],[a(=<),b(>=),c],Sorted).  
+% include a link for uri
+def_fancy_term(_,URI, _Options) -->
+	{ 
+	atom(URI),uri_is_global(URI) 
+	},
+	html(td(a(href(URI),URI))).
 
-sort_dicts(In,[],In) :- !.
-sort_dicts(In,[TmpKey|Keys],Sorted) :-
-	sort_dicts(In,Keys,TmpSorted),
-	(TmpKey =.. [Key,Order] -> sort(Key,Order,TmpSorted,Sorted) ; Sorted = TmpSorted).
+def_fancy_term(ColName,value(Term), Options) --> def_fancy_term(ColName,Term, Options).
 
-dicts_to_rows([],_,[]).
-dicts_to_rows([Dict|Dicts],Headers,[Row|Rows]) :-
-    dict_to_row(Dict,Headers,Row),
-    dicts_to_rows(Dicts,Headers,Rows).
-
-dict_to_row(Dict,Headers,Row) :-
-    var(Headers),!,
-    dict_pairs(Dict,Tag,Pairs),
-    pairs_keys_values(Pairs, Headers, Values),
-    Row =.. [Tag|Values].
-
-dict_to_row(Dict,Headers,Row) :-
-    \+var(Headers),!,
-    dict_pairs(Dict,Tag,Pairs),
-    pairs_keys_values(Pairs, Keys, Values),
-    findall(Value,(member(Key,Headers),nth1(I,Keys,Key),nth1(I,Values,Value)),NValues),
-    Row =.. [Tag|NValues].
-
-is_list_of_dicts([],_,_).
-is_list_of_dicts([Dict|Dicts],Tag,Keys) :-
-	is_dict(Dict,Tag),
-	dict_keys(Dict, Keys),
-	is_list_of_dicts(Dicts,Tag,Keys).
+% default
+def_fancy_term(ColName,Term, Options) -->
+	{ 
+		debug(fancy_table,"use default for ColName: ~w, Term:~w", [ColName,Term])
+	},
+	html(td(\term(Term, Options))).
 
 
 % create a hook in swish_render_table to render specific terms
 :- abolish(swish_render_table:row/4).
-swish_render_table:row([], _) --> [].
-swish_render_table:row([H|T], Options) -->
+swish_render_table:row(Row, Options) --> {debug(fancy_table,"using fancy_row: ~w",[Row])},fancy_row(Row,Options).
+
+
+fancy_row([],_) --> [].
+fancy_row([QCell|Cells], Options) -->
 	{
-		member(keys(Keys),Options),
-		length(Keys,N),
-		length(T,M),
-		I is N - M,
-		nth1(I,Keys,Key),
-		FancyTerm =.. [Key,H]
-		%debug(fancy_table,"fancy_term: ~w", [FancyTerm])
+		debug(fancy_table,"fancy_row: ~w", [QCell]),
+		QCell =.. [ColName,Cell]
 	},
-	fancy_term(FancyTerm,Options),
-	swish_render_table:row(T, Options).
-swish_render_table:row([H|T], Options) -->
-	html(td(\term(H, Options))),
-	swish_render_table:row(T, Options).
-swish_render_table:row([H|T], Options) -->
-	html(td(\term(H, []))),
-	swish_render_table:row(T, Options).
+	user_or_default_fancy_term(ColName,Cell,Options),
+	fancy_row(Cells, Options).
+
