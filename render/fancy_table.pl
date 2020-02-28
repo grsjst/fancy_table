fe29f4b524cf2e8a997f7ddf4f0a4927f07aadf5
Jozef Geurts <jpt.geurts@gmail.com>
2020-02-28 14:54:23 +0100

    dicts to rows

--- render/fancy_table.pl
+++ render/fancy_table.pl
@@ -21,14 +21,27 @@
 % :- use_gitty_file(test_fancy_term). % this comes from SWISH
 
 
-/** <module> SWISH fancy table renderer
-Includes fancy options for rendering tables using the standard table renderer
+/** <module> SWISH fancy table renderer for Dicts
 
-Options include:
-	- multuple key sorting
-	- special formatting for specific values
+	Includes fancy options for rendering tables that include:
+	- multiple key sorting
+	- special formatting for specific cell or column values
 	- download as xls 
+	
+	key sorting is specified through a header specification that is part of the render options.
+	It takes the format: header(Tag,[KeyA(Order),KeyB(Order),..]).
 
+	A number of specialised cell renders are provided (e.g. renndering dates, url, rdf resources). Additional
+	cell renders may be provide through the fancy_file(+File) parameter in the redner options. File refers
+	to a SWISH filename. This file inludes DCG rules of the format fancy_term(ColumnName,Term,Options)
+		ColumnName refers to the Key, Term is the term that is to be rendered, Options are the Renderer Options
+
+	e.g. fancy_term(profit,important(Value),Options) -->
+			html(td(b(\term(Term, Options)))).
+
+	is a directive to render the Value bold if it is marked important, and is presented under the profit column 
+
+	The renderer is based on the table renderer that is part of the core SWISH distribution
 */
 
 
@@ -37,50 +50,66 @@ Options include:
 %	Renders Rows as a fancy_table.
 term_rendering(Dicts, _Vars, Options) -->
 	{
-			% test for all dicts
-			to_rows(Dicts,Tag,Rows), % Rows = list of pairs -> to rows ()
-			match_header(Options,Tag,Rows,FancyHeader),!,
-			debug(fancy_table, "use fancy table - header:~w, options:~w",[FancyHeader,Options]),
-			header_keys(FancyHeader,Tag,SortKeys,Keys),
-			reorder_values(Rows,Keys,OrderedRows),
-			sort_rows(OrderedRows,SortKeys,SortedRows),
-			delete(Options,header(_),NOptions)
+		% debug(fancy_table, "try fancy table: ~p",[Dicts]),
+		to_rows(Dicts,Tag,Rows), % Rows = list of compounds -> to rows ()
+		match_header(Options,Rows,FancyHeader),!,
+		debug(fancy_table, "use fancy table - header:~w, options:~w",[FancyHeader,Options]),
+		header_keys(FancyHeader,Tag,SortKeys,Keys),
+		reorder_values(Rows,Keys,OrderedRows),
+		sort_rows(OrderedRows,SortKeys,SortedRows),
+		delete(Options,header(_),NOptions)
 	},
 	fancy_table_rendering(FancyHeader,SortedRows,NOptions). 
 
-to_rows([],_,[]).
+%!	to_rows(+Dicts:list(dict),-Tag:atom, -Rows:list(term)) is det.
+%	converts dicts to corresponding compound representation
+% 		Tag is the dict tag, which is represented as functor of the compound
+%			in case the Tag isn't an atom, "row" is used
+%		Key:Value are represented as pairs Key-Value
+%
+% 	e.g. to_rows([_{x:1,y:1,z:2},_{x:2,y:3,z:1}],T,Rs).
+to_rows([],Tag,[]) :- (var(Tag) -> Tag = row ; true),!.
 to_rows([Dict|Dicts],Tag,[Row|Rows]) :-
 	is_dict(Dict,Tag),
+	to_rows(Dicts,Tag,Rows),
 	dict_pairs(Dict,Tag,Pairs),
-	Row =.. [Tag,Pairs],
-	to_rows(Dicts,Tag,Rows).
+	Row =.. [Tag|Pairs].
 
-%! match_header(+Options,+Tag,+Rows,-Header) is nondet
-match_header(Options,Tag,Rows,Header) :-
-	common_keys(Rows,CommonKeys),
+%! 	match_header(+Options,+Rows,-Header) is nondet.
+%	from the Options select a header that matches the given Tag and the layout of the Rows 
+match_header(Options,Rows,Header) :-
+	common_keys(Rows,CommonTag,CommonKeys),
 	member(header(Header),Options),
-	header_keys(Header,Tag,_,HeaderKeys),
+	header_keys(Header,CommonTag,_,HeaderKeys),
 	subset(HeaderKeys,CommonKeys).
 
-% common_keys([row(a-3,c-1,b-2],row(b-2,a-2,c-2,d-0),row(a,b,c)])
+%!	common_keys(+Rows:list(term),-Tag:atom,-Keys:list(atom)) is det.
+%  	the list of keys that are present in all rows as wel as the tag that is
+% 	common to a Rows.
+% 
+% 	e.g. common_keys([row(a-3,c-1,b-2],row(b-2,a-2,c-2,d-0),row(a,b,c)],Tag,Keys)
 common_keys([],[]) :- !.
-common_keys([Row],Keys) :- Row =.. [_|Pairs],pairs_keys(Pairs,Keys).
-common_keys([Row|Rows],CommonKeys) :-
-	Row =.. [_|Pairs],
+common_keys([Row],CommonTag,Keys) :- Row =.. [CommonTag|Pairs],pairs_keys(Pairs,Keys).
+common_keys([Row|Rows],CommonTag,CommonKeys) :-
+	Row =.. [CommonTag|Pairs],
 	pairs_keys(Pairs,Keys),
-	common_keys(Rows,CKeys),
+	common_keys(Rows,CommonTag,CKeys),
 	intersection(Keys,CKeys,CommonKeys).
 
 header_keys(Header,Tag,SortKeys,HeaderKeys) :-
 	Header =.. [Tag|SortKeys],
 	findall(HKey,(member(SortKey,SortKeys),SortKey =.. [HKey,_]),HeaderKeys).
 
-% reorder_values([row(a-3,c-1,b-2),row(b-2,a-2,c-2),row(a,b,c)],Ordered)
+
+%!	reorder_value(+Rows:list(term),+Keys:list(atom),-OrderedRows:list(term)) is det.
+%	reposition the Key-Value pairs in Rows according to the scheme provide by Keys
+%
+% 	e.g. reorder_values([row(a-3,c-1,b-2),row(b-2,a-2,c-2),row(a-1,b-2,c-3)],[a,b,c],Ordered)
 reorder_values([],_,[]).
 reorder_values([Row|Rows],Keys,[OrderedRow|OrderedRows]) :-
-	Row =.. [_|Pairs],
+	Row =.. [Tag|Pairs],
 	order_pairs(Pairs,Keys,OrderedPairs),
-	OrderedRow =.. [_|OrderedPairs],
+	OrderedRow =.. [Tag|OrderedPairs],
 	reorder_values(Rows,Keys,OrderedRows).
 
 order_pairs(Pairs,[],Pairs) :- !.
@@ -88,18 +117,10 @@ order_pairs(Pairs,[K|Ks],[K-V|OrderedPairs]) :-
 	select(K-V,Pairs,RPairs),!,
 	order_pairs(RPairs,Ks,OrderedPairs).
 
-get_keys(Rows,Keys) :-
-	get_keys(Rows,1,Keys).
-get_keys(Rows,I,[]) :- forall(member(Row,Rows),(functor(Row,_,N), I > N)).
-get_keys(Rows,I,[Key|Keys]) :-
-	Rows = [FirstRow|_],
-	arg(I,FirstRow,Key-_),
-	forall(member(Row,Rows),arg(I,Row,Key-_)),
-	J is I + 1,
-	get_keys(Rows,J,Keys).
-
-
-% sort_rows([row(a-3,b-2,c-1),row(a-2,b-2,c-2),row(a-2,b-1,c-2)],[a(@>=),b(@=<)],Sorted)
+%!	sort_rows(+Rows:list(term),+SortKeys:list(term),-SortedRows:list(term)) is det.
+%	Sort rows on multiple columns provided through SortKeys.
+%		a Sortkey is of the format Key(Order), 
+% 	e.g. sort_rows([row(a-3,b-2,c-1),row(a-2,b-2,c-2),row(a-2,b-1,c-2)],[a(@>=),b(@=<)],Sorted)
 sort_rows(Rows,SortKeys,SortedRows) :-
 	get_keys(Rows,Keys),
 	sort_rows(Rows,Keys,SortKeys,SortedRows).
@@ -116,6 +137,16 @@ sort_rows(Rows,Keys,[SortKey|SortKeys],SortedRows) :-
 sort_rows(Rows,Keys,[_|SortKeys],SortedRows) :-
 	sort_rows(Rows,Keys,SortKeys,SortedRows).
 
+get_keys(Rows,Keys) :-
+	get_keys(Rows,1,Keys).
+get_keys(Rows,I,[]) :- forall(member(Row,Rows),(functor(Row,_,N), I > N)).
+get_keys(Rows,I,[Key|Keys]) :-
+	Rows = [FirstRow|_],
+	arg(I,FirstRow,Key-_),
+	forall(member(Row,Rows),arg(I,Row,Key-_)),
+	J is I + 1,
+	get_keys(Rows,J,Keys).
+
 fancy_table_rendering(Header,Rows,Options) -->
 	{
 		header_keys(Header,Tag,_,Keys),
@@ -129,19 +160,6 @@ fancy_table_rendering(Header,Rows,Options) -->
 		 	\table_term_rendering(Rows,_,[header(NHeader)|Options]),
 		 	\fancy_table_footer(Rows)		 	
 		 ])).
-
-fancy_table_rendering(Rows,Options) -->
-	{
-		% debug(fancy_table, "Tag: ~w, Keys:~w, Rows:~w, Options:~w, Header:~w",[Tag, Keys, Rows, Options,Header])
-	},
-	html(div([class(['export-dom']), style('display:inline-block'),
-		   'data-render'('Fancy Table without Header')
-		 ],
-		 [  
-		 	\table_term_rendering(Rows,_,Options),
-		 	\fancy_table_footer(Rows)		 	
-		 ])).
-
 
 fancy_table_footer([]) --> html(p("0 records")).
 fancy_table_footer(Rows) -->
@@ -182,8 +200,8 @@ atomize_row(Row,ARow) :-
 %	
 %	supported;
 % 	we renders dates as eg "16 Aug 2018"
-%	wiki_id include a clcikable link 
-%  	human friendly format for dates
+%	rdf resources are presented with their label
+%	url include a clcikable link 
 
 :- multifile swish_render_fancy_table:fancy_term/5.
 
@@ -193,6 +211,7 @@ user_or_default_fancy_term(ColName,Cell,Options) --> def_fancy_term(ColName,Cell
 def_fancy_term(_,Date, Options) -->
 	{   
 		Date = date(_,_,_),!,
+		debug(fancy_table,"date: ~w",[Date]),
 		format_time(string(DateStr),"%d %b %Y",Date)
 	},
 	html(td(\term(DateStr, Options))).
